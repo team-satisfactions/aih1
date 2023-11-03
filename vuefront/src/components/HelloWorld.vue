3f667c3673eeb4ef54be24d39295036eb57ddb27
amanoese <amanoese@gmail.com>
2023-11-03 23:11:14 +0900

    add animation

--- vuefront/src/components/HelloWorld.vue
+++ vuefront/src/components/HelloWorld.vue
@@ -2,11 +2,13 @@
   <div class="hello">
     <XyzTransitionGroup class="item-group" xyz="stagger-2 narrow-100%">
       <h1 class="square" v-if="spell">{{ spell }}</h1>
-      <h1>↓</h1>
+      <h1 class="square" v-if="spell">↓</h1>
       <h1 class="square" v-if="command">$ {{ command }}</h1>
-      <h1>↓</h1>
+      <h1 class="square" v-if="command">↓</h1>
       <h1 class="square" v-if="commandResult">{{ commandResult }}</h1>
     </XyzTransitionGroup>
+  </div>
+  <div class="overlay" :class="bcolor">
   </div>
 </template>
 
@@ -30,6 +32,7 @@ export default {
     setInterval(() => {
       anime.value = !anime.value
     }, 1000);
+    const bcolor = ref('')
     const spell = ref('')
     const command = ref('')
     const commandResult = ref('')
@@ -43,6 +46,7 @@ export default {
             clearTimeout(int_id)
             let data = res.data
             console.log(data)
+            bcolor.value = ''
             spell.value = ''
             command.value = ''
             commandResult.value = ''
@@ -57,33 +61,43 @@ export default {
       int_id = interval()
       let sussess = (data)=>{
         spell.value = data.spell
-        command.value = data.command
         const succeeded = data.succeeded
-        if(command.value && succeeded) {
-          console.log(int_id)
-          console.log(spell.value,command.value)
-          console.log(`command changed`)
-          setTimeout(()=>{
-            axios.post('http://localhost:8000/chant-spell').then(res => {
-              commandResult.value = res.data
-              console.log(res)
-              console.log(spell.value,command.value)
+        console.log({succeeded})
+        setTimeout(()=>{
+          command.value = data.command
+          if(succeeded) {
+            console.log(int_id)
+            console.log(spell.value,command.value)
+            console.log(`command changed`)
+            setTimeout(()=>{
+              axios.post('http://localhost:8000/chant-spell').then(res => {
+                commandResult.value = res.data
+                console.log(res)
+                console.log(spell.value,command.value)
+                setTimeout(() => {
+                  bcolor.value = 'green'
+                }, 1100);
 
-              setTimeout(() => {
-                int_id = interval()
-              }, 2000);
-            }).catch(err => {
-              console.log(err)
-            });
-          },2000)
-        } else {
-          setTimeout(() => {
-            int_id = interval()
-          }, 2000);
-        }
+                setTimeout(() => {
+                  int_id = interval()
+                }, 2000);
+              }).catch(err => {
+                console.log(err)
+              });
+            },2000)
+          } else {
+            commandResult.value = 'x'
+            setTimeout(() => {
+              bcolor.value = 'red'
+            }, 1100);
+            setTimeout(() => {
+              int_id = interval()
+            }, 2000);
+          }
+        }, 2000);
       }
     })
-    return { spell, command, commandResult,anime }
+    return { spell, command, commandResult, anime, bcolor }
   }
 }
 
@@ -108,5 +122,18 @@ a {
 .item-group {
   --xyz-translate-y: -350%;
   --xyz-ease: cubic-bezier(0.5, -1.5, 0.5, 1.5);
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left:0;
+  right:0;
+  bottom:0;
+}
+.red {
+  background-color: rgba(255,0,0,0.2);
+}
+.green {
+  background-color: rgba(0,255,0,0.2);
 }
 </style>
