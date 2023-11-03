108cf8b5afd154024d839f1a59f49f048ddb18a7
goya813 <wasabibeef@gmail.com>
2023-11-03 17:04:01 +0900

    色々実装

--- server/app.py
+++ server/app.py
@@ -1,12 +1,25 @@
 import subprocess
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class Spell(BaseModel):
@@ -20,17 +33,18 @@ class DecodedSpell(BaseModel):
 
 reserved_decoded_spell: Optional[DecodedSpell] = None
 
-allow_commands = ["eject"]
+allow_commands = ["eject", "ls", "echo"]
 
 
 @app.post("/reserve-spell")
-def receive_spell(spell: Spell) -> Optional[DecodedSpell]:
+def receive_spell(spell: Spell) -> DecodedSpell:
+    global reserved_decoded_spell
     command = f"PATH=$PATH:/Users/goya/.nvm/versions/node/v21.0.0/bin /Users/goya/.nvm/versions/node/v21.0.0/bin/chant -d {spell.text}"
     decoded_command = subprocess.run(command, capture_output=True, text=True, shell=True).stdout.replace("\n", "")
 
     run_command = decoded_command.split(" ")[0]
     if run_command not in allow_commands:
-        return None
+        raise HTTPException(status_code=400, detail=f"not allow command: {run_command}")
 
     reserved_decoded_spell = DecodedSpell(spell=spell.text, command=decoded_command)
 
@@ -38,18 +52,21 @@ def receive_spell(spell: Spell) -> Optional[DecodedSpell]:
 
 
 @app.get("/reserved-spell")
-def read_item() -> Optional[DecodedSpell]:
+def reserved_spell() -> DecodedSpell:
+    global reserved_decoded_spell
     if reserved_decoded_spell is None:
-        return None
+        raise HTTPException(status_code=400, detail="not reserved spell")
 
     return reserved_decoded_spell
 
 
 @app.post("/chant-spell")
-def read_item() -> Optional[DecodedSpell]:
+def chant_spell() -> DecodedSpell:
+    global reserved_decoded_spell
     if reserved_decoded_spell is None:
-        return None
+        raise HTTPException(status_code=400, detail="not reserved spell")
 
     # コマンドを実行する
+    subprocess.run(reserved_decoded_spell.command, shell=True)
 
     return reserved_decoded_spell
