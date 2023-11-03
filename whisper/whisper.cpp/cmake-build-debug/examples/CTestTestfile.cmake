a2ac1db840a0209b2e76206bf6607f9bbd12a105
Yuta Hinokuma <higumachan@users.noreply.github.com>
2023-11-03 16:05:03 +0900

    add files

---
+++ whisper/whisper.cpp/cmake-build-debug/examples/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yuta/ghq/github.com/team-satisfactions/aih1/whisper/whisper.cpp/examples
+# Build directory: /Users/yuta/ghq/github.com/team-satisfactions/aih1/whisper/whisper.cpp/cmake-build-debug/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("main")
+subdirs("stream")
+subdirs("command")
+subdirs("bench")
+subdirs("quantize")
+subdirs("talk")
+subdirs("talk-llama")
+subdirs("lsp")
